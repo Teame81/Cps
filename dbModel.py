c5cb2fe5b5e41039a3a61d623a32bf9bf29c0027
Timmie <timmie.nilsson@gmail.com>
2019-11-28 13:27:12 +0100

    Data base modeling done for noe

--- dbModel.py
+++ dbModel.py
@@ -9,7 +9,7 @@ basedir = os.path.abspath(os.path.dirname(__file__)) # __file__ is the name of t
 ######################## INITIATE DATABASE ########################
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+os.path.join(basedir,'data.sqlite.sql')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:aik123@localhost:5432/cps'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 
@@ -25,17 +25,19 @@ class A_premises(db.Model):
     __tablename__ = 'premises'
     id = db.Column(db.Integer,primary_key=True)
     # premises id.
-    premises_id = db.Column(db.Text)
+    premises_id = db.Column(db.String(50))
     # This could company name or more.
-    short_description = db.Column(db.Text)
+    short_description = db.Column(db.String(120))
     # One premises can have several devices.
         # ex: column_name = db.relationship('Class to make relationship to', backref = 'this table name')
-    devices = db.relationship('Device', backref = 'premises', lazy = 'dynamic')
+    devices = db.relationship('Device', backref = 'A_premises', lazy = 'dynamic')
         
-    # Initate data into the premises table
-    def __init__ (self, premises_id, short_description):
+   # Initate data into the premises table
+    def __init__ (self, premises_id, short_description, devices):
         self.premises_id = premises_id
         self.short_description = short_description
+        self.devices = devices
+
 
     def __repr__(self):
         if self.devices:
@@ -52,12 +54,12 @@ class A_premises(db.Model):
 class Device(db.Model):
     __tablename__ = 'devices'
     device_id = db.Column(db.Integer, primary_key=True)
-    position = db.Column(db.Integer, db.ForeignKey('premises.premises_id'))
+    position = db.Column(db.Integer, db.ForeignKey('premises.id'))
     visitors = db.relationship('Click' , backref = 'devices', lazy = 'dynamic')
-    def __init__(self,device_id, premises_id):
+    def __init__(self,device_id, position):
         self.device_id = device_id
-        self.premises_id = premises_id
-
+        self.position = position
+        
     def __repr__(self):
         pass
 #-------------DEVICES END-------------#
@@ -66,7 +68,7 @@ class Device(db.Model):
 class Click(db.Model):
     __tablename__ = 'clicks'
     id = db.Column(db.Integer, primary_key=True)
-    pass_through = db.Column(db.Integer, db.ForeignKey('devices.visitors'))
+    pass_through = db.Column(db.Integer, db.ForeignKey('devices.device_id'))
     
     def __init__(self, pass_through):
         self.pass_through = pass_through
